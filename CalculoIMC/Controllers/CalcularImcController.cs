0d48e12fdc004025b0d9f7b168bb0f1b292f2d24
NataliaLopes <natalialopescig@gmail.com>
2019-06-06 21:45:44 -0300

    Calculadora de imc feito no padrão mvc

---
+++ CalculoIMC/Controllers/CalcularImcController.cs
@@ -0,0 +1,55 @@
+﻿using CalculoIMC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculoIMC.Controllers
+{
+    public class CalcularImcController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string altura, string peso)
+        {
+            try
+            {
+                Pessoa pessoa = new Pessoa();
+
+                //Pega os parâmetros digitados pelo usuário
+                pessoa.altura = Convert.ToDecimal(altura.Replace(".", ","));
+                pessoa.peso = Convert.ToDecimal(peso.Replace(".", ","));
+
+                //Calcula o imc
+                pessoa.imc = pessoa.calculaImc(pessoa);
+
+                //Redireciona para a tela de resultado
+                return RedirectToAction("Resultado", new { imc = pessoa.imc });
+            }
+            catch (Exception)
+            {
+                ViewBag.msgErro = "Dados inválidos...";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Resultado(decimal imc)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            pessoa.imc = imc;
+
+            //A partir do imc calculado gera o resultado do usuário para mostrar na tela
+            pessoa.resultado = pessoa.montaResultado(imc);
+
+            return View(pessoa);
+        }
+    }
+}
---
+++ CalculoIMC/Models/Pessoa.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculoIMC.Models
+{
+    public class Pessoa
+    {
+        public decimal altura { get; set; }
+        public decimal peso { get; set; }
+        public decimal imc { get; set; }
+        public string resultado { get; set; }
+
+        /// <summary>
+        /// Realiza o cálculo do imc
+        /// </summary>
+        /// <param name="pessoa"></param>
+        /// <returns>retorna o valor do imc</returns>
+        public decimal calculaImc(Pessoa pessoa)
+        {
+            try
+            {
+                decimal resultadoImc = 0;
+
+                //Calcula o imc
+                resultadoImc = Math.Round((pessoa.peso / (pessoa.altura * pessoa.altura)), 2);
+
+                return resultadoImc;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// A partir do valor do imc define o resultado
+        /// </summary>
+        /// <param name="imc"></param>
+        /// <returns>retorna o resultado da pessoa</returns>
+        public string montaResultado(decimal imc)
+        {
+            try
+            {
+                string resultado = "";
+
+                if (imc < Convert.ToDecimal(18.5))
+                {
+                    resultado = "abaixo do peso";
+                }
+                else if (imc >= Convert.ToDecimal(18.5) && imc <= Convert.ToDecimal(24.9))
+                {
+                    resultado = "peso normal";
+                }
+                else if (imc >= Convert.ToDecimal(25) && imc <= Convert.ToDecimal(29.9))
+                {
+                    resultado = "sobrepeso";
+                }
+                else if (imc >= 30)
+                {
+                    resultado = "obesidade";
+                }
+
+                return resultado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
